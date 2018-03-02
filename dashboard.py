6683fc4d3f0ea59ccfcdb8908b7ae4668b9fbab5
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-02 11:28:20 +0530

    First commiut

---
+++ dashboard.py
@@ -0,0 +1,71 @@
+import pygame
+from pygame.locals import *
+
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+TITLEBAR_STRING = "Intelli Sense Dashboard"
+
+pygame.init()
+clock = pygame.time.Clock()
+game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+pygame.display.set_caption(TITLEBAR_STRING)
+white = (255,255,255)
+black = (0,0,0)
+car_width = 73
+
+# Loading images
+carImg = pygame.image.load("racecar.png")
+
+def car(x,y):
+	game_display.blit(carImg, (x,y))
+
+
+def text_objects(text, font):
+	textSurface = font.render(text, True, black)
+	return textSurface, textSurface.get_rect()
+
+def message_display(text):
+	largeText = pygame.font.Font("freesansbold.ttf", 115)
+	TextSurf, TextRect = text_objects(text, largeText)
+	TextRect.center = ((DISPLAY_WIDTH/2), (DISPLAY_HEIGHT/2))
+
+
+# The main game loop
+def game_loop():
+	x = (DISPLAY_WIDTH * 0.45)
+	y = (DISPLAY_HEIGHT * 0.8)
+
+	x_change = 0
+
+	gameExit = False
+
+	while not gameExit:
+		for event in pygame.event.get():
+			print(event)
+			if event.type == QUIT:
+				gameExit = True
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					x_change = -5
+				if event.key == pygame.K_RIGHT:
+					x_change = 5
+
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+					x_change = 0
+		x += x_change
+
+		game_display.fill(white)
+		car(x,y)
+
+		if x > DISPLAY_WIDTH - 
+
+		pygame.display.update()
+		clock.tick(60)
+
+
+
+game_loop()
+pygame.quit()
+quit()
