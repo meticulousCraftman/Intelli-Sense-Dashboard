e47f3ee5ffa07a4df15ac6312002c06d08d27433
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-02 17:00:45 +0530

    Added the Speedometer component to the scree. Still the code is very basic and primitive. Needs a lot of structure.

--- dashboard.py
+++ dashboard.py
@@ -2,29 +2,44 @@ import pygame
 from pygame.locals import *
 import time
 
+import components
+
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
+WHITE = (255,255,255)
 TITLEBAR_STRING = "Intelli Sense Dashboard"
 
 pygame.init()
 clock = pygame.time.Clock()
 game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption(TITLEBAR_STRING)
-white = (255,255,255)
-black = (0,0,0)
-car_width = 73
+
+
+x = (DISPLAY_WIDTH * 0.3)
+y = (DISPLAY_HEIGHT * 0.2)
+
 
 # Loading images
-ringImg = pygame.image.load("Ring.tif")
+ringImg = pygame.image.load("./images/ring.tif")
+a1 = pygame.image.load("./images/1.tif")
 ringImg = pygame.transform.scale2x(ringImg)
+a1 = pygame.transform.scale2x(a1)
 
-background = pygame.image.load("background.jpg")
+
+background = pygame.image.load("./images/background.jpg")
+
+speedometer = components.speedometer.Speedometer((x,y))
 
 def bg():
 	game_display.blit(background, (0,0))
 
 def ring(x,y):
 	game_display.blit(ringImg, (x,y))
+
+
+all_sprites_list = pygame.sprite.Group()
+all_sprites_list.add(speedometer)
+
 
 
 def text_objects(text, font):
@@ -43,10 +58,14 @@ def message_display(text):
 def crash():
 	message_display("You Crashed")
 
+
+
+
+
+
 # The main game loop
 def game_loop():
-	x = (DISPLAY_WIDTH * 0.3)
-	y = (DISPLAY_HEIGHT * 0.2)
+	
 
 	x_change = 0
 
@@ -54,28 +73,25 @@ def game_loop():
 
 	while not gameExit:
 		for event in pygame.event.get():
-			print(event)
+			# print(event)
 			if event.type == QUIT:
 				pygame.quit()
 				quit()
 
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_LEFT:
-					x_change = -5
-				if event.key == pygame.K_RIGHT:
-					x_change = 5
 
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-					x_change = 0
-		x += x_change
-
-		game_display.fill(white)
+		# Componenets that needs to present on the screen
+		game_display.fill(WHITE)
 		bg()
 		ring(x,y)
-
-		if x > (DISPLAY_WIDTH - car_width) or x < 0:
-			crash()
+		all_sprites_list.update()
+		
+		game_display.blit(speedometer.image, (x,y))
+		
+		for u in range(1,36):
+			speedometer.update(u)
+			game_display.blit(speedometer.image, (x,y))
+			pygame.display.update()
+			time.sleep(0.5)
 
 		pygame.display.update()
 		clock.tick(60)
