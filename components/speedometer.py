c576e08ea6df2ab20dc86f36dbcb078ab6b79a7a
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-02 23:35:29 +0530

    Added a sprite class for showing the empty ring on the screen.

--- components/speedometer.py
+++ components/speedometer.py
@@ -1,6 +1,22 @@
 import pygame
 
 
+class EmptyRing(pygame.sprite.Sprite):
+	
+	def __init__(self,position=None):
+		super().__init__()
+
+		self.image = pygame.image.load("./images/ring.tif")
+		self.image = pygame.transform.scale2x(self.image)
+		self.rect = self.image.get_rect()
+		if position is not None:
+			self.rect.x = position[0]
+			self.rect.y = position[1]
+
+
+
+
+
 class Speedometer(pygame.sprite.Sprite):
 	"""
 	This class represents the speedomter present on the screen
@@ -17,13 +33,17 @@ class Speedometer(pygame.sprite.Sprite):
 			self.rect.y = position[1]
 
 
+	def set_speed(self, speed):
+		self.speed = speed
 
-	def update(self, speed):
+
+
+	def update(self):
 		"""
 		This reads the value from the sensor and updates
 		the board accordingly.
 		"""
-		self.active_count = speed
-		image_url = "./images/%s.tif" % str(speed)
+		self.active_count = self.speed
+		image_url = "./images/%s.tif" % str(self.speed)
 		self.image = pygame.image.load(image_url)
 		self.image = pygame.transform.scale2x(self.image)
