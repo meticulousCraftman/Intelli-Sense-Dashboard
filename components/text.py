c815f610f0dca3a7e5d3c893b5074501a8e33794
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-03 03:00:36 +0530

    Used for adding text on the display

--- components/text.py
+++ components/text.py
@@ -1,2 +1,17 @@
 import pygame
 
+COLORS = {
+	"WHITE":(255,255,255)
+}
+
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, COLORS["WHITE"])
+    return textSurface, textSurface.get_rect()
+
+def message_display(display,text,position,size=40):
+    largeText = pygame.font.Font('./fonts/OpenSans-Light.ttf',size)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = (position[0], position[1])
+    display.blit(TextSurf, TextRect)
