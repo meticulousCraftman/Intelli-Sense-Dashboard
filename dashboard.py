eb821c10969da7c37cafd2a9676deeb01494c901
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-02 23:46:09 +0530

    Gave some structure to the code and added the sprite classes to the Group class.

--- dashboard.py
+++ dashboard.py
@@ -4,71 +4,48 @@ import time
 
 import components
 
+
+# Global settings
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
 WHITE = (255,255,255)
 TITLEBAR_STRING = "Intelli Sense Dashboard"
 
+
+# Application related settings
 pygame.init()
 clock = pygame.time.Clock()
 game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption(TITLEBAR_STRING)
-
-
 x = (DISPLAY_WIDTH * 0.3)
 y = (DISPLAY_HEIGHT * 0.2)
 
 
-# Loading images
-ringImg = pygame.image.load("./images/ring.tif")
-a1 = pygame.image.load("./images/1.tif")
-ringImg = pygame.transform.scale2x(ringImg)
-a1 = pygame.transform.scale2x(a1)
-
 
 background = pygame.image.load("./images/background.jpg")
 
-speedometer = components.speedometer.Speedometer((x,y))
 
+speedometerComp = components.speedometer.Speedometer((x,y))
+emptyRingComp = components.speedometer.EmptyRing((x,y))
+speedometerComp.set_speed(26)
+
+
+# This will ke a list of all the sprites present on the screen
+all_sprites_list = pygame.sprite.Group()
+
+
+# Adding speedometer to the group of sprites
+all_sprites_list.add(emptyRingComp)
+all_sprites_list.add(speedometerComp)	
+
+
+# For displaying the background
 def bg():
 	game_display.blit(background, (0,0))
-
-def ring(x,y):
-	game_display.blit(ringImg, (x,y))
-
-
-all_sprites_list = pygame.sprite.Group()
-all_sprites_list.add(speedometer)
-
-
-
-def text_objects(text, font):
-	textSurface = font.render(text, True, white)
-	return textSurface, textSurface.get_rect()
-
-def message_display(text):
-	largeText = pygame.font.Font("./fonts/OpenSans-Light.ttf", 115)
-	TextSurf, TextRect = text_objects(text, largeText)
-	TextRect.center = ((DISPLAY_WIDTH/2), (DISPLAY_HEIGHT/2))
-	game_display.blit(TextSurf, TextRect)
-	pygame.display.update()
-	time.sleep(2)
-	game_loop()
-
-def crash():
-	message_display("You Crashed")
-
-
-
-
 
 
 # The main game loop
 def game_loop():
-	
-
-	x_change = 0
-
 	gameExit = False
 
 	while not gameExit:
@@ -82,16 +59,8 @@ def game_loop():
 		# Componenets that needs to present on the screen
 		game_display.fill(WHITE)
 		bg()
-		ring(x,y)
 		all_sprites_list.update()
-		
-		game_display.blit(speedometer.image, (x,y))
-		
-		for u in range(1,36):
-			speedometer.update(u)
-			game_display.blit(speedometer.image, (x,y))
-			pygame.display.update()
-			time.sleep(0.5)
+		all_sprites_list.draw(game_display)
 
 		pygame.display.update()
 		clock.tick(60)
