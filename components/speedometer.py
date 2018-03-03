50aa4ff9ab08cfe5499e4ec92d0f41fb9e843b59
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-03 18:01:07 +0530

    Added a method to scale the speedometer to our desired size.

--- components/speedometer.py
+++ components/speedometer.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 
 class EmptyRing(pygame.sprite.Sprite):
@@ -7,10 +8,15 @@ class EmptyRing(pygame.sprite.Sprite):
 		super().__init__()
 
 		self.image = pygame.image.load("./images/ring.tif")
-		self.image = pygame.transform.scale2x(self.image)
 		self.rect = self.image.get_rect()
 		if position is not None:
 			self.rect.center = (position[0], position[1])
+
+	def scale(self,times=1):
+		w,h = self.image.get_size()
+		neww = math.floor(w*times)
+		newh = math.floor(h*times)
+		self.image = pygame.transform.scale(self.image, (neww, newh))
 
 
 
@@ -21,9 +27,8 @@ class Speedometer(pygame.sprite.Sprite):
 
 	def __init__(self,position=None):
 		super().__init__()
-
+		self.scale_times = 1
 		self.image = pygame.image.load("./images/1.tif")
-		self.image = pygame.transform.scale2x(self.image)
 		self.rect = self.image.get_rect()
 		if position is not None:
 			self.rect.center = (position[0], position[1])
@@ -31,7 +36,6 @@ class Speedometer(pygame.sprite.Sprite):
 
 	def set_speed(self, speed):
 		self.speed = speed
-
 
 
 	def update(self):
@@ -42,4 +46,13 @@ class Speedometer(pygame.sprite.Sprite):
 		self.active_count = self.speed
 		image_url = "./images/%s.tif" % str(self.speed)
 		self.image = pygame.image.load(image_url)
-		self.image = pygame.transform.scale2x(self.image)
+		self.scale(self.scale_times)
+		# self.image = pygame.transform.scale2x(self.image)
+	
+
+	def scale(self,times=1):
+		self.scale_times = times
+		w,h = self.image.get_size()
+		neww = math.floor(w*times)
+		newh = math.floor(h*times)
+		self.image = pygame.transform.scale(self.image, (neww, newh))
