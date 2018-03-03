b3141eb2f8e131b91ab35a2db9ea1cfb220b71c1
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-03 17:10:28 +0530

    2 classes for Increase, decrease button. And a function for handling click events on the screen.

---
+++ components/buttons.py
@@ -0,0 +1,45 @@
+import pygame
+import math
+
+class IncreaseButton(pygame.sprite.Sprite):
+	
+	def __init__(self,position=None):
+		super().__init__()
+		self.position = position
+		self.image = pygame.image.load("./images/increase.tif")
+		self.rect = self.image.get_rect()
+		if position is not None:
+			self.rect.center = (position[0], position[1])
+
+	def scale(self,times=1):
+		w,h = self.image.get_size()
+		neww = math.floor(w*times)
+		newh = math.floor(h*times)
+		self.image = pygame.transform.scale(self.image, (neww, newh))
+
+
+class DecreaseButton(pygame.sprite.Sprite):
+	
+	def __init__(self,position=None):
+		super().__init__()
+		self.position = position
+		self.image = pygame.image.load("./images/decrease.tif")
+		self.rect = self.image.get_rect()
+		if position is not None:
+			self.rect.center = (position[0], position[1])
+
+	def scale(self,times=1):
+		w,h = self.image.get_size()
+		neww = math.floor(w*times)
+		newh = math.floor(h*times)
+		self.image = pygame.transform.scale(self.image, (neww, newh))
+
+
+def GenericButton(buttonSurface, action=None):
+	mouse = pygame.mouse.get_pos()
+	click = pygame.mouse.get_pressed()
+
+	w,h = buttonSurface.image.get_size()
+	if buttonSurface.rect.x + w > mouse[0] > buttonSurface.rect.x and buttonSurface.rect.y + h > mouse[1] > buttonSurface.rect.y:
+		if click[0] == 1 and action != None:
+			action()
