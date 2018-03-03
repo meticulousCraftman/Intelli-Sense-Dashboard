a9690e630991649a216a9ff1dfffd4bb8a6cb958
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-03 17:15:30 +0530

    Added an exit button on the dashboard screen.

--- dashboard.py
+++ dashboard.py
@@ -29,6 +29,8 @@ background = pygame.image.load("./images/background.jpg")
 
 speedometerComp = components.speedometer.Speedometer((x,y-100))
 emptyRingComp = components.speedometer.EmptyRing((x,y-100))
+exitButton = components.buttons.DecreaseButton((770,20))
+exitButton.scale(0.75)
 speedometerComp.set_speed(26)
 
 
@@ -39,7 +41,7 @@ all_sprites_list = pygame.sprite.Group()
 # Adding speedometer to the group of sprites
 all_sprites_list.add(emptyRingComp)
 all_sprites_list.add(speedometerComp)
-
+all_sprites_list.add(exitButton)
 
 
 # For displaying the background
@@ -47,28 +49,51 @@ def bg():
 	game_display.blit(background, (0,0))
 
 
+def action():
+	components.text.message_display(game_display,"Exit",(x,y-25-100),100)
+	gameExit = True
+	pygame.quit()
+
+
 # The main game loop
 def game_loop():
-	gameExit = False
 
-	while not gameExit:
-		for event in pygame.event.get():
-			# print(event)
-			if event.type == QUIT:
-				pygame.quit()
-				quit()
+	try:
+		gameExit = False
+
+		while not gameExit:
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					gameExit = True
+					break
 
 
-		# Componenets that needs to present on the screen
-		game_display.fill(WHITE)
-		bg()
-		all_sprites_list.update()
-		all_sprites_list.draw(game_display)
-		components.text.message_display(game_display,"22.34",(x,y-25-100),50)
-		components.text.message_display(game_display,"KMPH",(x,y+25-100),35)
+			# Componenets that needs to present on the screen
+			game_display.fill(WHITE)
+			bg()
+			all_sprites_list.update()
 
-		pygame.display.update()
-		clock.tick(60)
+			mouse = pygame.mouse.get_pos()
+
+			# For Speedometer
+			all_sprites_list.draw(game_display)
+			components.text.message_display(game_display,"22.34",(x,y-25-100),50)
+			components.text.message_display(game_display,"KMPH",(x,y+25-100),35)
+
+			# Odometer
+			components.text.message_display(game_display,"24 KM",(x,y+60),35)
+
+			# Exit Button
+			components.buttons.GenericButton(exitButton, action)
+
+
+			pygame.display.update()
+			clock.tick(60)
+	except KeyboardInterrupt:
+		pygame.quit()
+		quit()
+	pygame.quit()
+	quit()
 
 
 
