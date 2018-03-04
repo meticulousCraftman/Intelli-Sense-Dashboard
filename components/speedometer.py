76685eaf1b910be24ad1e009adf86ffdac09949e
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-05 00:37:34 +0530

    New class for updating the text value as well as the rings at the same time using the value passed as the argument.

--- components/speedometer.py
+++ components/speedometer.py
@@ -12,13 +12,34 @@ COLORS = {
 
 speedometerComp = primitive.rings.ActiveRings(0, 48, (x-300,y-100))
 speedometerComp.scale(1.75)
-speedometerComp.setValue(1)
+speedometerComp.desc = "Active Rings"
+
 emptyRingSpeedometerComp = primitive.rings.EmptyRing((x-300,y-100))
 emptyRingSpeedometerComp.scale(1.75)
+
 speedText = primitive.text.Text("22.34",50, COLORS["WHITE"],(x-250,y-70))
+speedText.desc = "Magnitude"
+
 speedUnitText = primitive.text.Text("kmph",40, COLORS["WHITE"],(x-250,y-30))
 
-Speedometer = pygame.sprite.Group()
+class SpeedometerClass(pygame.sprite.Group):
+
+	def __init__(self):
+		super().__init__()
+
+	def update(self, value=0):
+		self.value = value
+		listOfSprites = self.sprites()
+		for x in listOfSprites:
+			if hasattr(x, "desc"):
+				if x.desc=="Magnitude":
+					x.changeText(str(self.value))
+				elif x.desc=="Active Rings":
+					x.setValue(self.value)
+
+		super().update()
+
+Speedometer = SpeedometerClass()
 Speedometer.add(emptyRingSpeedometerComp)
 Speedometer.add(speedometerComp)
 Speedometer.add(speedText)
