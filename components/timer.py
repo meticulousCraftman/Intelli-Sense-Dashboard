2e851d309f8e1a34bc72e232b86d43148c5dc364
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-04 11:41:01 +0530

    Added text to the timer component.

--- components/timer.py
+++ components/timer.py
@@ -10,9 +10,16 @@ COLORS = {
 	"WHITE":(255,255,255)
 }
 
-emptyRingTimeComp = primitive.rings.EmptyRing((x+50,y-130))
-timeComp = primitive.rings.ActiveRings((x+50,y-130))
+BASE_POSITION = (450,110)
+
+
+emptyRingTimeComp = primitive.rings.EmptyRing(BASE_POSITION)
+timeComp = primitive.rings.ActiveRings(BASE_POSITION)
+componentName = primitive.text.Text("Timer",25, COLORS["WHITE"],(BASE_POSITION[0],BASE_POSITION[1]-90))
+time = primitive.text.Text("12:54",25, COLORS["WHITE"],BASE_POSITION)
 
 Timer = pygame.sprite.Group()
 Timer.add(emptyRingTimeComp)
 Timer.add(timeComp)
+Timer.add(componentName)
+Timer.add(time)
