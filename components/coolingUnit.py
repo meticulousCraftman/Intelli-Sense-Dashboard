c1418790ca23bfb310410b8cbda90d3f5da5be42
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-05 02:09:51 +0530

    Buttons text and active rings all the primitive elements work in perfect harmony.

--- components/coolingUnit.py
+++ components/coolingUnit.py
@@ -1,5 +1,6 @@
 import pygame
 import primitive
+import time
 
 pygame.init()
 
@@ -10,13 +11,54 @@ COLORS = {
 BASE_POSITION = (700,110)
 
 emptyRingcoolingComp = primitive.rings.EmptyRing(BASE_POSITION)
-coolingUnitComp = primitive.rings.ActiveRings(0,100,BASE_POSITION)
-rpmIncButton = primitive.buttons.IncreaseButton((BASE_POSITION[0]+65, BASE_POSITION[1]+80))
-rpmDecButton = primitive.buttons.DecreaseButton((BASE_POSITION[0]-65,BASE_POSITION[1]+80))
-componentName = primitive.text.Text("Cooling Unit",25, COLORS["WHITE"],(BASE_POSITION[0],BASE_POSITION[1]-90))
-coolingPercentage = primitive.text.Text("10 %",30, COLORS["WHITE"],BASE_POSITION)
 
-CoolingUnit = pygame.sprite.Group()
+coolingUnitComp = primitive.rings.ActiveRings(0,100,BASE_POSITION)
+coolingUnitComp.desc = "Active Rings"
+
+rpmIncButton = primitive.buttons.IncreaseButton((BASE_POSITION[0]+65, BASE_POSITION[1]+80))
+rpmIncButton.desc = "Increase"
+
+rpmDecButton = primitive.buttons.DecreaseButton((BASE_POSITION[0]-65,BASE_POSITION[1]+80))
+rpmDecButton.desc = "Decrease"
+
+componentName = primitive.text.Text("Cooling Unit",25, COLORS["WHITE"],(BASE_POSITION[0],BASE_POSITION[1]-90))
+
+coolingPercentage = primitive.text.Text("0 %",30, COLORS["WHITE"],BASE_POSITION)
+coolingPercentage.desc = "Magnitude"
+
+
+
+class CoolingUnitClass(pygame.sprite.Group):
+
+	def __init__(self):
+		self.value = 0
+		super().__init__()
+
+	def update(self):
+		listOfSprites = self.sprites()
+		for x in listOfSprites:
+			if hasattr(x, "desc"):
+				if x.desc=="Magnitude":
+					x.changeText(str(self.value)+" %")
+				elif x.desc=="Active Rings":
+					x.setValue(self.value)
+				elif x.desc=="Increase":
+					x.clicked(self.rpmInc)
+				elif x.desc=="Decrease":
+					x.clicked(self.rpmDec)
+		super().update()
+
+	def rpmInc(self):
+		if self.value < 100:
+			self.value += 10
+
+	def rpmDec(self):
+		if self.value > 0:
+			self.value -= 10
+
+
+
+CoolingUnit = CoolingUnitClass()
 CoolingUnit.add(emptyRingcoolingComp)
 CoolingUnit.add(coolingUnitComp)
 CoolingUnit.add(rpmDecButton)
