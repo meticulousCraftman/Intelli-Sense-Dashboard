eeebc59a01887fb9da92c43759cad7aff54342e0
Apoorva Singh <apoorvasingh157@gmail.com>
2018-03-04 12:31:32 +0530

    Added the namespace for slider element.

--- primitive/__init__.py
+++ primitive/__init__.py
@@ -2,7 +2,9 @@ try:
 	import rings
 	import text
 	import buttons
+	import slider
 except ImportError:
 	from primitive import rings
 	from primitive import text
 	from primitive import buttons
+	from primitive import slider
